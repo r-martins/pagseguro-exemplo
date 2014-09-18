df7b7d2e16189b6d1dfda08f03d81e4dc7b0371a
Ricardo Martins <ricardo@ricardomartins.info>
2014-09-18 18:05:38 -0300

    Modo sandbox - versao pronta pra ir pro ar

--- reset.sh
+++ reset.sh
@@ -1,3 +1,3 @@
-mysql -h localhost -u pagseguro -ppagseguro_pass -D pagseguro < /root/public_pagseguro/2014-09-17_220558_pagseguro.sql
+mysql -h localhost -u pagseguro -ppagseguro_pass -D pagseguro < /root/public_pagseguro/database.sql
 mysql -h localhost -u pagseguro -ppagseguro_pass -D pagseguro -e "update core_config_data set value='http://pagseguro-exemplo.ricardomartins.net.br/' where path='web/unsecure/base_url';"
 mysql -h localhost -u pagseguro -ppagseguro_pass -D pagseguro -e "update core_config_data set value='http://pagseguro-exemplo.ricardomartins.net.br/' where path='web/secure/base_url';"
